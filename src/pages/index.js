fde93122669dcc10cd0dbf549ce7f82351850209
Kaio Rodrigo <kaiorodrigocoder@gmail.com>
2022-03-27 03:19:29 -0300

    Dark theme na tela inicial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div>
+      <div className="dark:bg-slate-900 dark:text-white dark:h-screen">
         <div>
           <h1 className="font-black">Loja</h1>
           <p>Loja</p>
