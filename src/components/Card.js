a582ff5491981b3a6f9c2d07942af7320b650a27
Kaio Rodrigo <kaiorodrigocoder@gmail.com>
2022-03-28 02:13:06 -0300

    Onclick no botao Edit

--- src/components/Card.js
+++ src/components/Card.js
@@ -73,6 +73,7 @@ export default function Card(props) {
           <button
             type="button"
             className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={openModal}
           >
             <PencilIcon className="-ml-1 mr-2 h-5 w-5 " aria-hidden="true" />
             Edit
